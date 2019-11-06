3217404d188f7bc02406713a74701bfe68d4b5e9
Zeke Hart <heirloch@gmail.com>
2019-11-06 15:39:01 -0500

    intial 'working' version using useState and useEffect to get and update the match info

--- src/components/MatchHistory.js
+++ src/components/MatchHistory.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import matchHistory from '../services/MatchHistory.js'
 import axios from 'axios'
 
-const matches = getMatchHistory()
-console.log('q', typeof (matches), matches)
-console.log(matchHistory)
+// const matches = getMatchHistory()
+// console.log('q', typeof (matches), matches)
+// console.log(matchHistory)
+// let [matchOne, getMatches] = useState()
 
 function MatchHistory () {
-  return <h1>ABC</h1>
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get('https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/4tFF6inlqX5x4fN2KoBPtWXvbAVV-b6C09jqoX9wDc9NIeE_rTtcYhtwDmryRxMKCDEp6hN9qTgzrQ/ids?api_key=RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6')
+      .then(response => response.data)
+      .then(data => setData(data))
+  }, [])
+
+  return <div>
+
+    <h1>Matches</h1>
+    {/* <button onClick={getMatchHistory}>boop</button> */}
+    <p>{data}</p>
+  </div>
 }
 
-async function getMatchHistory () {
-  //   const dailyApi = 'RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6'
-  // const mH = await axios.get(`https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/4tFF6inlqX5x4fN2KoBPtWXvbAVV-b6C09jqoX9wDc9NIeE_rTtcYhtwDmryRxMKCDEp6hN9qTgzrQ/ids?api_key=RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6`)
-  let joey = await axios.get('https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/4tFF6inlqX5x4fN2KoBPtWXvbAVV-b6C09jqoX9wDc9NIeE_rTtcYhtwDmryRxMKCDEp6hN9qTgzrQ/ids?api_key=RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6').then(res => { return res.data })
-  // return mH.data[0]
-  joey = await Array.from(joey)
-  console.log('joey', typeof (joey[0]), joey[0])
-
-  return joey[0]
-}
+// async function getMatchHistory () {
+//   //   const dailyApi = 'RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6'
+//   // const mH = await axios.get(`https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/4tFF6inlqX5x4fN2KoBPtWXvbAVV-b6C09jqoX9wDc9NIeE_rTtcYhtwDmryRxMKCDEp6hN9qTgzrQ/ids?api_key=RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6`)
+//   const joey = await axios.get('https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/4tFF6inlqX5x4fN2KoBPtWXvbAVV-b6C09jqoX9wDc9NIeE_rTtcYhtwDmryRxMKCDEp6hN9qTgzrQ/ids?api_key=RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6').then(res => { return res.data })
+//   // return mH.data[0]
+//   getMatches(matchOne = joey[0])
+//   // return joey[0]
+// }
 
 export default MatchHistory
