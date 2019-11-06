dfa9aadb89a58ab626e2db178cd04a4c82a4a0f9
Zeke Hart <heirloch@gmail.com>
2019-11-06 16:51:52 -0500

    maps out matches so they're slightly more readable

--- src/components/MatchHistory.js
+++ src/components/MatchHistory.js
@@ -18,18 +18,10 @@ function MatchHistory () {
   return <div>
 
     <h1>Matches</h1>
-    {/* <button onClick={getMatchHistory}>boop</button> */}
-    <p>{data}</p>
+    {data.map((match, id) => {
+      return <p key={id}>{match}</p>
+    })}
   </div>
 }
-
-// async function getMatchHistory () {
-//   //   const dailyApi = 'RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6'
-//   // const mH = await axios.get(`https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/4tFF6inlqX5x4fN2KoBPtWXvbAVV-b6C09jqoX9wDc9NIeE_rTtcYhtwDmryRxMKCDEp6hN9qTgzrQ/ids?api_key=RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6`)
-//   const joey = await axios.get('https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/4tFF6inlqX5x4fN2KoBPtWXvbAVV-b6C09jqoX9wDc9NIeE_rTtcYhtwDmryRxMKCDEp6hN9qTgzrQ/ids?api_key=RGAPI-6c402bb1-7fc0-42be-87ad-ab97f90369f6').then(res => { return res.data })
-//   // return mH.data[0]
-//   getMatches(matchOne = joey[0])
-//   // return joey[0]
-// }
 
 export default MatchHistory
